4c829d6a0e259d2eedf6b64a5537c91d08e8f8cb
Inakir <sysadmin@sec.tamu.edu>
2016-02-11 16:39:51 -0600

    Added classes
    Added empty classes of rows, tables, schemas, etc.

---
+++ src/snowshoe/DBTable.java
@@ -0,0 +1,25 @@
+public class DBTable
+{
+    String name;
+    DBRows rows;
+    Attributes attributes;
+
+    void rename(String rename)
+    {
+        
+  
+    }
+    
+    void update_on_disk()
+    {
+        
+  
+    }
+    
+    DBTable select(Condition c)
+    {
+        return //some DBTable
+    }
+    
+    
+}
